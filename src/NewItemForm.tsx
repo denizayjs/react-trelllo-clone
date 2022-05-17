b0bb13e6f6abb6ec27be993988519c8cb91c0e85
denizayjs <denizay@std.iyte.edu.tr>
2022-05-18 02:04:58 +0300

    added autofocus to input

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useFocus } from "./useFocus";
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles";
 interface NewItemFormProps {
   onAdd(text: string): void;
@@ -6,10 +7,11 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
-
+  const inputRef = useFocus();
   return (
     <NewItemFormContainer>
       <NewItemInput
+        ref={inputRef}
         value={text}
         onChange={(e) => setText(e.target.value)}
       ></NewItemInput>
