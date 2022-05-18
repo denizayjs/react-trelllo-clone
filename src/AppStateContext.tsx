c94856e5378995e2144f6bfd2cd149ad79e3c286
denizayjs <denizay@std.iyte.edu.tr>
2022-05-18 13:26:16 +0300

    implement state management with ReactContext

---
+++ src/AppStateContext.tsx
@@ -0,0 +1,49 @@
+import React, { createContext } from "react";
+
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+interface Task {
+  id: string;
+  text: string;
+}
+interface List {
+  id: string;
+  text: string;
+  tasks: Task[];
+}
+interface AppStateContextProps {
+  state: AppState;
+}
+
+const appData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "0", text: "Generate app scaffold" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "1", text: "Learn Typescript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "2", text: "Begin to use static typing" }],
+    },
+  ],
+};
+
+export interface AppState {
+  lists: List[];
+}
+
+export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
+  return (
+    <AppStateContext.Provider value={{ state: appData }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
